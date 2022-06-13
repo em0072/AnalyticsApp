a86162259ab9969ec2ede8bd07d77c0c628ad245
Evgeny Mitko <em0072@gmail.com>
2022-06-13 11:11:40 +0200

    Initial Commit

---
+++ AnalyticsApp/AnalyticsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsAppApp.swift
+//  AnalyticsApp
+//
+//  Created by Evgeny Mitko on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnalyticsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnalyticsApp
+//
+//  Created by Evgeny Mitko on 13/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
